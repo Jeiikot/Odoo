cadbfe06d0cadb003e55168afd786675e224cc73
Jeiikot <jeiikot21@gmail.com>
2020-06-05 18:03:01 -0500

    [ADD] basic template excel

--- odoo13/cv/models/cv_excel.py
+++ odoo13/cv/models/cv_excel.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
+
 import base64
 from io import BytesIO
 
@@ -18,9 +20,7 @@ class CvReportExcelWizard(models.TransientModel):
 
         workbook = xlsxwriter.Workbook(fp)
 
-        worksheet = workbook.add_worksheet('CV Report')
-
-        worksheet.write(1, 1, 'test')
+        self.get_data(workbook)
 
         workbook.close()
         file_download = base64.b64encode(fp.getvalue())
@@ -39,6 +39,63 @@ class CvReportExcelWizard(models.TransientModel):
             'target': 'new',
             'context': dict_context,
         }
+
+    def get_data(self, workbook):
+        worksheet = workbook.add_worksheet('CV Report')
+        # Template format
+        heading_format = workbook.add_format({'align': 'center',
+                                              'valign': 'vcenter',
+                                              'bold': True, 'size': 14})
+        cell_text_format_1 = workbook.add_format({'align': 'center',
+                                                  'bold': True, 'size': 9,
+                                                  })
+        cell_text_format_2 = workbook.add_format({'align': 'center',
+                                                  'bold': False, 'size': 9,
+                                                  })
+        worksheet.set_column('A:A', 20)
+        worksheet.set_column('B:B', 20)
+        worksheet.set_column('C:C', 20)
+        worksheet.set_column('D:D', 20)
+        worksheet.set_column('E:E', 20)
+        worksheet.set_column('F:F', 20)
+        worksheet.set_column('G:G', 20)
+        worksheet.set_column('H:H', 20)
+        worksheet.set_column('I:I', 20)
+        worksheet.set_column('J:J', 20)
+        worksheet.set_column('K:K', 20)
+        worksheet.set_column('L:L', 20)
+        worksheet.set_column('M:M', 20)
+        worksheet.set_column('N:N', 20)
+
+        # Write
+
+        today = datetime.today()
+        worksheet.merge_range('A1:F2', 'Curriculum Vitae', heading_format)
+        worksheet.write('F3', "%s %s %s" % (today.day, today.strftime("%B"), today.year), cell_text_format_1)
+
+        cv_academic_lines = self.env['hr.employee.cv'].search([('employee_ref.id', '=', self.employee_ref.id)])
+        temp = 0
+        for line in cv_academic_lines:
+            worksheet.write(6, 0, "Employee", cell_text_format_1)
+            worksheet.write(6, 1, "Nationality", cell_text_format_1)
+            worksheet.write(6, 2, "Document Type", cell_text_format_1)
+            worksheet.write(6, 3, "No.", cell_text_format_1)
+            worksheet.write(6, 4,  "Gender", cell_text_format_1)
+            worksheet.write(6, 5,  "Job Position", cell_text_format_1)
+            worksheet.write(6, 6,  "Email", cell_text_format_1)
+            worksheet.write(6, 7,  "Mobile Phone", cell_text_format_1)
+
+            worksheet.write(7, 0 + temp, "%s" % line.name if line.name!=False else '', cell_text_format_2)
+            worksheet.write(7, 1 + temp, "%s" % line.country_id.name if line.country_id!=False else '', cell_text_format_2)
+            worksheet.write(7, 2 + temp, "%s" % line.document_type if line.document_type!=False else '', cell_text_format_2)
+            worksheet.write(7, 3 + temp, "%s" % line.identification_id if line.identification_id!=False else '', cell_text_format_2)
+            worksheet.write(7, 4 + temp, "%s" % line.gender if line.gender!=False else '', cell_text_format_2)
+            worksheet.write(7, 5 + temp, "%s" % line.job_id.name if line.job_id.name!=False else '', cell_text_format_2)
+            worksheet.write(7, 6 + temp, "%s" % line.email if line.email!=False else '', cell_text_format_2)
+            worksheet.write(7, 7 + temp, "%s" % line.mobile_phone if line.mobile_phone!=False else '', cell_text_format_2)
+            temp+=1
+
+
 
 
 class CvReportExcel(models.TransientModel):
