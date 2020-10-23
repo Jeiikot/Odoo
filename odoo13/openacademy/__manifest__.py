f95a8ed6db5e7e801bb72190ef0c1df7041c5e5e
Jeiikot <jeiikot21@gmail.com>
2020-10-22 20:34:44 -0500

    [ADD] Dashboard in openacademy module. Define a dashboard containing the graph view you created, the sessions calendar view and a list view of the courses (switchable to a form view). This dashboard sholud be available through a menuitem in the menu, and automaticaly displayed in the web client when the OpenAcademy main menu is selected.

--- odoo13/openacademy/__manifest__.py
+++ odoo13/openacademy/__manifest__.py
@@ -21,7 +21,10 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': [
+        'base',
+        'board',
+    ],
 
     # always loaded
     'data': [        
@@ -31,7 +34,7 @@
         'views/openacademy.xml',
         'views/partner.xml',
         'reports/reports.xml',
-
+        'views/session_board.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
