f03ee20c2103ec4c6d973a2a6493948524783928
Jeiikot <jeiikot21@gmail.com>
2020-10-28 19:14:16 -0500

    [ADD] calling methods

--- odoo13/openacademy/External API/doc/xml-rpc.py
+++ odoo13/openacademy/External API/doc/xml-rpc.py
@@ -7,12 +7,45 @@ db = input('Database:')
 username = input('User:')
 password = input("Password:")
 
-# The xmlrpc/2/common endpoint provides meta-calls which don’t require authentication,
-# such as the authentication itself or fetching version information.
+"""
+    Logging in
+
+    Odoo requires users of the API to be authenticated before they can query most data.
+    
+    The xmlrpc/2/common endpoint provides meta-calls which don’t require authentication, such as the 
+    authentication itself or fetching version information. To verify if the connection information is 
+    correct before trying to authenticate, the simplest call is to ask for the server’s version. 
+    The authentication itself is done through the authenticate function and returns a user identifier (uid) 
+    used in authenticated calls instead of the login.
+"""
 common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
 # print(common.version())
 
-# The authentication itself is done through the authenticate function and returns a user
-# identifier (uid) used in authenticated calls instead of the login.
 uid = common.authenticate(db, username, password, {})
 # print(uid)
+
+"""
+    Calling methods
+
+The second endpoint is xmlrpc/2/object, is used to call methods of odoo models via the execute_kw RPC function.
+
+Each call to execute_kw takes the following parameters:
+
+    the database to use, a string
+    the user id (retrieved through authenticate), an integer
+    the user’s password, a string
+    the model name, a string
+    the method name, a string
+    an array/list of parameters passed by position
+    a mapping/dict of parameters to pass by keyword (optional)
+
+For instance to see if we can read the res.partner model we can call check_access_rights with operation passed by 
+position and raise_exception passed by keyword (in order to get a true/false result rather than true/error):
+"""
+name_model ="openacademy.session"
+#name_model = input("Name of model:")
+
+models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+# models.execute_kw(db, uid, password,
+#     'res.partner', 'check_access_rights',
+#     ['read'], {'raise_exception': False})
