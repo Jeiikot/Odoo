416797f7b10e8ca9453fd9e44e7a3fdd0039071e
Jeiikot <jeiikot21@gmail.com>
2020-11-19 20:18:09 -0500

    [CREATE] request and stage models in Technical Service module.

--- odoo13/technical_service/models/models.py
+++ odoo13/technical_service/models/models.py
@@ -1,18 +1,35 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class technical_service(models.Model):
-#     _name = 'technical_service.technical_service'
-#     _description = 'technical_service.technical_service'
+class technicalServiceStage(models.Model):
+    """ Model for case stages. This models the main stages of a Technical Service Request management flow. """
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    _name = 'technical_service.stage'
+    _description = 'Technical Service Stage'
+    _order = 'sequence, id'
+
+    name = fields.Char('Name', required=True, translate=True)
+    sequence = fields.Integer('Sequence', default=20)
+    done = fields.Boolean('Request Done')
+
+class technicalServiceRequest(models.Model):
+    _name = 'technical_service.request'
+
+    _description = 'Technical Service Request'
+
+    @api.returns('self')
+    def _default_stage(self):
+        return self.env['technical_service.stage'].search([], limit=1)
+
+    name = fields.Char('Name', required=True, translate=True)
+    company_id = fields.Many2one('res.company', string='Company',
+                                 default=lambda self: self.env.company)
+    description = fields.Text('Description')
+    priority = fields.Selection([('0', 'Very Low'), ('1', 'Low'), ('2', 'Normal'), ('3', 'High')], string='Priority')
+    request_date = fields.Date('Request Date', tracking=True, default=fields.Date.context_today,
+                               help="Date requested for the technical service to happen")
+    stage_id = fields.Many2one('technical_service.stage', string='Stage', ondelete='restrict',
+                               tracking=True, default=_default_stage, copy=False)
+
