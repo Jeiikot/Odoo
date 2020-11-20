10219e8ad95f6e00a100eed8b9064be269506fa3
Jeiikot <jeiikot21@gmail.com>
2020-11-19 22:04:57 -0500

    [ALTER] Technical Service Request. - Add category service model. - Add fields in request model.

--- odoo13/technical_service/models/models.py
+++ odoo13/technical_service/models/models.py
@@ -14,6 +14,14 @@ class technicalServiceStage(models.Model):
     sequence = fields.Integer('Sequence', default=20)
     done = fields.Boolean('Request Done')
 
+class technicalServiceCategory(models.Model):
+    _name = 'technical_service.category'
+    _description = 'Technical Service Category'
+
+    name = fields.Char('Name', required=True, translate=True)
+    code = fields.Char('Code', required=True)
+
+
 class technicalServiceRequest(models.Model):
     _name = 'technical_service.request'
 
@@ -24,12 +32,25 @@ class technicalServiceRequest(models.Model):
         return self.env['technical_service.stage'].search([], limit=1)
 
     name = fields.Char('Name', required=True, translate=True)
-    company_id = fields.Many2one('res.company', string='Company',
-                                 default=lambda self: self.env.company)
     description = fields.Text('Description')
-    priority = fields.Selection([('0', 'Very Low'), ('1', 'Low'), ('2', 'Normal'), ('3', 'High')], string='Priority')
+    priority = fields.Selection([
+        ('0', 'Very Low'),
+        ('1', 'Low'),
+        ('2', 'Normal'),
+        ('3', 'High')], string='Priority')
+
     request_date = fields.Date('Request Date', tracking=True, default=fields.Date.context_today,
                                help="Date requested for the technical service to happen")
+
+    schedule_date = fields.Datetime('Scheduled Date', required=True,
+        help="Date the Technical Service team plans the service.  It should not differ much from the Request Date. ")
+    start_date = fields.Datetime('Start Date')
+    end_date = fields.Datetime('End Date')
+
+    company_id = fields.Many2one('res.company', string='Company',
+                                 default=lambda self: self.env.company)
+    category_id = fields.Many2one('technical_service.category', string='Service Category', required=True)
+    user_id = fields.Many2one('res.users', string='Technician', tracking=True, required=True)
     stage_id = fields.Many2one('technical_service.stage', string='Stage', ondelete='restrict',
                                tracking=True, default=_default_stage, copy=False)
 
